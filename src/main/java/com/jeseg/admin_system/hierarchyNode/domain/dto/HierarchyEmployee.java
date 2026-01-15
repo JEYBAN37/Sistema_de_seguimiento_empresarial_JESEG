aefbb6611f3c54589c0794dede723117fc99a523
JEYBAN37 <jeyban37@gmail.com>
2026-01-14 21:27:57 -0500

    jeysgegu feat creacion de jerarquias

--- src/main/java/com/jeseg/admin_system/hierarchyNode/domain/dto/HierarchyEmployee.java
+++ src/main/java/com/jeseg/admin_system/hierarchyNode/domain/dto/HierarchyEmployee.java
@@ -1,12 +1,16 @@
 package com.jeseg.admin_system.hierarchyNode.domain.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor // VITAL para Jackson
+@AllArgsConstructor
 @Builder
 public class HierarchyEmployee {
     private String name;
-    private Integer parent;
+    private String parent; // En tu JSON viene como "1", así que String está bien
     private Integer quantity;
 }
