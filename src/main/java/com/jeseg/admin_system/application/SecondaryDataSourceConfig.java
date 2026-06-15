b8d3bd5232124713e3d47825fcb0ba2f55ed4ad1
JEYBAN37 <jeyban37@gmail.com>
2026-06-15 10:21:22 -0500

    jeysgegu fix: se quita el tunel ssh

--- src/main/java/com/jeseg/admin_system/application/SecondaryDataSourceConfig.java
+++ src/main/java/com/jeseg/admin_system/application/SecondaryDataSourceConfig.java
@@ -31,7 +31,6 @@ import java.util.Map;
         transactionManagerRef = "secondaryTransactionManager"
 )
 public class SecondaryDataSourceConfig {
-    private Session sshSession;
 
     @Bean
     @ConfigurationProperties(prefix = "spring.datasource.secondary")
@@ -64,39 +63,5 @@ public class SecondaryDataSourceConfig {
             EntityManagerFactory entityManagerFactory) {
 
         return new JpaTransactionManager(entityManagerFactory);
-    }
-
-    /**
-     * 🚀 ARRANQUE AUTOMÁTICO DEL TÚNEL
-     * @PostConstruct asegura que el túnel se abra ANTES de que Hibernate intente conectar
-     */
-    @PostConstruct
-    public void openSSHTunnel() {
-
-        try {
-            JSch jsch = new JSch();
-            // Cambia los datos por tus credenciales reales de MochaHost
-            sshSession = jsch.getSession("agsolutic", "195.250.27.25", 22);
-            sshSession.setPassword("Query*AP5");
-
-            java.util.Properties config = new java.util.Properties();
-            config.put("StrictHostKeyChecking", "no");
-            sshSession.setConfig(config);
-
-            // 🚀 LOS LATIDOS: Configura el Keep-Alive en milisegundos (30 segundos)
-            sshSession.setServerAliveInterval(30000);
-            // Si falla 3 veces consecutivas, recién ahí se da por muerta la conexión
-            sshSession.setServerAliveCountMax(3);
-
-            System.out.println("🔄 Iniciando túnel SSH con persistencia Keep-Alive...");
-            sshSession.connect();
-
-            // Mapeo del puerto local (ej. 3307) hacia el 3306 del servidor de MochaHost
-            sshSession.setPortForwardingL(3307, "127.0.0.1", 3306);
-            System.out.println("✅ Túnel SSH establecido en el puerto local 3307");
-
-        } catch (Exception e) {
-            System.err.println("❌ Error al levantar el túnel SSH: " + e.getMessage());
-        }
     }
 }
